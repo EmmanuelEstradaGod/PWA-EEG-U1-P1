4d8ed389c6f7725701b174f44fcc330ec67caf3e
EmmanuelEstradaGod <20183ti079@utez.edu.mx>
2021-12-04 20:51:07 -0600

    .

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let contextSW = "/PWA-EEG-U1-P1-/sw.js"
 let url = window.location.href;
 console.log("Hola Mundo desde app.js");
-
+console.log(`url: ${url}`);
 if (navigator.serviceWorker) {
     if (url.includes("localhost")) {
         contextSW = "/sw.js";
